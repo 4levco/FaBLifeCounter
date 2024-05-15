8794ed3d2491393bbfc4e8765129a68d4617d669
4levco <broedner.levin.lb@gmail.com>
2024-05-15 17:39:00 +0200

    menu

--- app/src/main/java/com/example/fablifecounter/MainActivity.kt
+++ app/src/main/java/com/example/fablifecounter/MainActivity.kt
@@ -1,7 +1,13 @@
 package com.example.fablifecounter
 
+import android.media.Image
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.PopupMenu
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -60,10 +66,71 @@ class MainActivity : AppCompatActivity() {
             healthtextp2.text = healthp2.toString()
         }
 
+        private fun showImageSelectionDialog() {
+            val fragmentManager = supportFragmentManager
+            val fragmentTransaction = fragmentManager.beginTransaction()
+            val imageSelectionFragment = ImageSelectionFragment()
+            fragmentTransaction.replace(R.id.fragment_container, imageSelectionFragment)
+            fragmentTransaction.addToBackStack(null)
+            fragmentTransaction.commit()
+        }
+
+        val menuButton = findViewById<ImageButton>(R.id.menu)
+
+        menuButton.setOnClickListener {
+            val popupMenu = PopupMenu(this, menuButton)
+            val inflater = menuInflater
+            inflater.inflate(R.menu.change_hero, popupMenu.menu)
+
+            popupMenu.setOnMenuItemClickListener { menuItem ->
+                val itemId = menuItem.itemId
+                if (itemId == R.id.change_hero) {
+                    showImageSelectionDialog()
+                    return@setOnMenuItemClickListener true
+                }
+
+                false
+            }
+
+            popupMenu.show()
+        }
+
+        /*
+        val imageOptions = listOf("Bravo", "Dorinthea", "Katsu", "Rhinar")
+
+        val imageResources = mapOf(
+            "Bravo" to  R.drawable.hero_bravo,
+            "Dorinthea" to R.drawable.hero_dorinthea,
+            "Katsu" to R.drawable.hero_katsu,
+            "Rhinar" to R.drawable.hero_rhinar
+        )
+
+        menuButton.setOnClickListener {
+            val popupMenu = PopupMenu(this,menuButton)
+
+            for (option in imageOptions) {
+                popupMenu.menu.add(Menu.NONE, option.hashCode(), Menu.NONE, option)
+            }
+
+            popupMenu.setOnMenuItemClickListener { menuItem ->
+                val selectedOption = menuItem.title.toString()
+                val imageID = imageResources[selectedOption] ?:
+                return@setOnMenuItemClickListener false
+
+                val selectedImage = resources.getDrawable(imageID)
+                val imageView = findViewById<ImageView>(R.id.backgroundp1)
+                imageView.setImageDrawable(selectedImage)
+
+                true
+            }
+            popupMenu.show()
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        */
     }
 }
